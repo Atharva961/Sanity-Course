63e63bbf90e3de5f240746d40e910010764d09fa
Atharva961 <atharvaag2002@gmail.com>
2023-01-23 09:37:42 +0530

    Sanity course video 3 completed

---
+++ schemas/author.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'author',
+    type: 'document',
+    title: 'Author',
+
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        }
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import author from "./author";
+import blog from "./blog";
+
+export const schemaTypes = [blog, author]
